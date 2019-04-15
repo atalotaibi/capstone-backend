77fd0a2de0c822e644b91a8c84270dadf64a12d6
atalotaibi <alotaibiatallah@gmail.com>
2019-04-15 18:14:37 +0300

    set up models

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Major, Question, Answer
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Major)
+admin.site.register(Question)
+admin.site.register(Answer)
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_expert = models.BooleanField(default=True)
+
+
+class Major(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Question(models.Model):
+    q_text = models.TextField()
+    ANSWEREd = (
+        ('T', 'true'),
+        ('F', 'false'),
+    )
+    answer_status = models.CharField(
+        max_length=2,
+        choices=ANSWEREd,
+        default='F',
+    )
+    APPROVED = (
+        ('T', 'true'),
+        ('F', 'false'),
+    )
+    answer_status = models.CharField(
+        max_length=2,
+        choices=APPROVED,
+        default='F',
+    )
+    major = models.ForeignKey(
+        Major, related_name='questions', default=1, on_delete=models.CASCADE)
+
+
+class Answer(models.Model):
+    q_text = models.TextField()
+    question = models.ForeignKey(
+        Question, related_name='answers', default=1, on_delete=models.CASCADE)
