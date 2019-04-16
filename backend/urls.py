e313a4c35f6f6b72d24013564d1a598ca6781e55
Mohammed Alotaibi <imohammed.mg@gmail.com>
2019-04-16 16:10:06 +0300

    Authentication completed

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-
+from rest_framework_jwt.views import obtain_jwt_token
+from .views import (
+    UserCreateAPIView
+)
 
 urlpatterns = [
+     ## Auth ##
+    path('login/', obtain_jwt_token, name='login'),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
 
 ]
